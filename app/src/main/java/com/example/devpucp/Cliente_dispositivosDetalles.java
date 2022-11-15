4a162b62be3e65456959cdb9b979a8391e26adc0
KatherineObregon <a20181823@pucp.edu.pe>
2022-11-14 22:45:25 -0500

    vistas home cliente avance

---
+++ app/src/main/java/com/example/devpucp/Cliente_dispositivosDetalles.java
@@ -0,0 +1,17 @@
+package com.example.devpucp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+
+public class Cliente_dispositivosDetalles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente_dispositivos_detalles);
+
+
+    }
+}
--- app/src/main/java/com/example/devpucp/HomeCliente.java
+++ app/src/main/java/com/example/devpucp/HomeCliente.java
@@ -3,6 +3,11 @@ package com.example.devpucp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
 public class HomeCliente extends AppCompatActivity {
 
@@ -11,4 +16,31 @@ public class HomeCliente extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_cliente);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_cerrar_sesion, menu);
+        return true;
+    }
+    public void btnMenuCerrarsesion(MenuItem menuItem){
+        Log.d("msg", "btn cerrar sesion");
+        View view = findViewById(R.id.btn_cerrarsesion);
+        PopupMenu popupMenu = new PopupMenu(this,view);
+        popupMenu.getMenuInflater().inflate(R.menu.sub_menu_logout,popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(menuItem1 -> {
+            switch (menuItem1.getItemId()){
+                case R.id.btn_menu_logout:
+                    Log.d("msg", "btn_cerrar sesion presionado");
+                    return true;
+                default:
+                    return false;
+
+            }
+
+        });
+        popupMenu.show();
+
+
+    }
+
 }
