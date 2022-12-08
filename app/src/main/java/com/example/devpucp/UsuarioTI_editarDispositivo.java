ad118d53edc37bc59e5dfb490071053c9d00f0ff
KatherineObregon <a20181823@pucp.edu.pe>
2022-12-08 01:09:35 -0500

    CRUD dispositivo completo

--- app/src/main/java/com/example/devpucp/UsuarioTI_editarDispositivo.java
+++ app/src/main/java/com/example/devpucp/UsuarioTI_editarDispositivo.java
@@ -2,20 +2,45 @@ package com.example.devpucp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewSwitcher;
+
+import com.bumptech.glide.Glide;
+import com.example.devpucp.Entities.Dispositivo;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class UsuarioTI_editarDispositivo extends AppCompatActivity {
 
-    ImageButton beforeButtonEditar, nextButtonEditar;
-    ImageSwitcher imageSwitcherDisp;
+    FirebaseDatabase firebaseDatabase;
+    StorageReference storageRef;
 
-    int index=0;
-    int imagenesid[]={R.drawable.ic_profile_foreground, R.drawable.ic_logo_pucp_celeste_foreground, R.drawable.ic_devices_foreground};
+    Dispositivo dispositivo = new Dispositivo();
+    AutoCompleteTextView tipoTV;
+    ImageView imageView;
+
+    TextInputEditText marca ;
+    TextInputEditText caracteristicas ;
+    TextInputEditText accesorios ;
+    TextInputEditText stock ;
+
+    String imageUrl;
+    DatabaseReference dispositivosRef;
+
 
 
     @Override
@@ -23,45 +48,73 @@ public class UsuarioTI_editarDispositivo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario_ti_editar_dispositivo);
 
-        beforeButtonEditar=findViewById(R.id.btn_UsuarioTI_before);
-        nextButtonEditar=findViewById(R.id.btn_UsuarioTI_next);
-        imageSwitcherDisp= findViewById(R.id.UsuarioTI_imageSwitcherDispEditar);
+        Intent intent = getIntent();
+        dispositivo = (Dispositivo) intent.getSerializableExtra("dispositivoDetalleTI");
 
-        beforeButtonEditar.setOnClickListener(new View.OnClickListener() {
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        dispositivosRef = firebaseDatabase.getReference().child("dispositivos").child(dispositivo.getKey());
+        storageRef = FirebaseStorage.getInstance().getReference().child(dispositivo.getKey());
+
+        marca = findViewById(R.id.UsuarioTI_inputEditarDispMarca);
+        caracteristicas = findViewById(R.id.UsuarioTI_inputEditarDispCaracteristicas);
+        accesorios = findViewById(R.id.UsuarioTI_inputEditarDispAccesorios);
+        stock = findViewById(R.id.UsuarioTI_inputEditarDispStock);
+        imageView = findViewById(R.id.imageView2);
+        tipoTV = findViewById(R.id.textInputTipoEditarOpciones);
+
+        //TODO SELECTOR INICIO
+
+
+        String[] dirigidoA = new String[]{"Laptop", "Tableta", "Celular", "Monitor"};
+        ArrayAdapter<String> adapterTipo = new ArrayAdapter<>(this, R.layout.drop_down_item, dirigidoA);
+        tipoTV.setText(dispositivo.getTipo());
+        tipoTV.setAdapter(adapterTipo);
+
+        tipoTV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                --index;
-                if (index<0){
-                    index=imagenesid.length-1;
-                }
-                imageSwitcherDisp.setImageResource(imagenesid[index]);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Toast.makeText(Admin_EventosAnadir.this, dirigidoaTV.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
+        //TODO SELECTOR FIN
 
-        nextButtonEditar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                index++;
-                if (index==imagenesid.length){
-                    index=0;
-                }
-                imageSwitcherDisp.setImageResource(imagenesid[index]);
-            }
-        });
+        if(dispositivo.getFotoUrl()!=null){
+            Glide.with(UsuarioTI_editarDispositivo.this).load(dispositivo.getFotoUrl()).into(imageView);
 
-        imageSwitcherDisp.setFactory(new ViewSwitcher.ViewFactory() {
-            @Override
-            public View makeView() {
+        }
+        marca.setText(dispositivo.getMarca());
+        caracteristicas.setText(dispositivo.getCaracteristicas());
+        accesorios.setText(dispositivo.getAccesorios());
+        stock.setText(dispositivo.getStock());
 
-                ImageView imageView = new ImageView(getApplicationContext());
-                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                imageView.setMaxWidth(250);
-                imageView.setMaxHeight(250);
 
-                return imageView;
-            }
-        });
 
-        imageSwitcherDisp.setImageResource(imagenesid[index]);
     }
+
+    public void actualizarDispositivo(View view){
+
+        String marcaNuevaStr = marca.getText().toString();
+        String caracteristicasNuevasStr = caracteristicas.getText().toString();
+        String accesoriosNuevoStr = accesorios.getText().toString();
+        String stockNuevoStr = stock.getText().toString();
+        String tipoNuevo = tipoTV.getText().toString();
+
+        dispositivo.setMarca(marcaNuevaStr);
+        dispositivo.setTipo(tipoNuevo);
+        dispositivo.setCaracteristicas(caracteristicasNuevasStr);
+        dispositivo.setAccesorios(accesoriosNuevoStr);
+        dispositivo.setStock(stockNuevoStr);
+
+        dispositivosRef.setValue(dispositivo).addOnSuccessListener(unused -> {
+            Toast.makeText(UsuarioTI_editarDispositivo.this, "Se actualizó dispositivo correctamente.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, UsuarioTI_gestionarDispositivos.class);
+            startActivity(intent);
+        });
+
+
+    }
+
+
+
 }
